c89630f6b4d2624411e27c6fb0fef22734f5c636
= <=>
2021-05-08 06:59:59 +0530

    To analyse the given text for freqency of words

--- word_freq_counter.py
+++ word_freq_counter.py
@@ -1,14 +1,18 @@
+# importing the text in docx format only
 import textract
 text = textract.process('sample.docx')
 text = text.decode("utf-8")
 
+#inserting the entire document to a list
 sentence_list = []
 sentence_list.append(text)
 
+# splitting the sentences for spaces and removing . and ,
 sentence_list = sentence_list[0].split()
 sentence_list = [word.replace(".", "") for word in sentence_list]
 sentence_list = [word.replace(",", "") for word in sentence_list]
 
+# converting to a list for getting keys(words) and values(freq) pairs
 dict = {}
 def dict_maker(list):
     for i in range (1, len(sentence_list)):
@@ -21,16 +25,13 @@ def dict_maker(list):
 dict_maker(sentence_list)
 print(dict) 
 
-
+# gettting the graph on matplotlib
 from matplotlib import pyplot as plt
 from matplotlib.widgets import Slider
-
-
 plt.style.use('fivethirtyeight')
 words_x = dict.keys()
 freq_y = dict.values()
 plt.bar(words_x, freq_y, color='k', label='Words_freq_count')
-
 plt.xlabel('Words used in the given text')
 plt.ylabel('Freq of words')
 plt.title('Text analyzer')
